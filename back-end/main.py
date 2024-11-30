f85c8a247e667d875fcd774ef2dd3978d22094ec
Joseph Driscoll <driscoll2507@gmail.com>
2024-11-30 22:45:02 +0000

    feat: added products json file as well as cosine sim route handling using fastapi. added images to more products

--- back-end/main.py
+++ back-end/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from content_based_filtering.tfidf_cosine_similarity_products import get_recommendations
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Specify frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 # Define the request body structure
 class ProductRequest(BaseModel):
@@ -10,8 +20,9 @@ class ProductRequest(BaseModel):
 
 @app.post("/product-cosine-sim")
 async def product_cosine_sim(request: ProductRequest):
+    print(request)
     try:
-        # print(request.product_name)
+        print(request.product_name)
         product_name = request.product_name
         recommendations = get_recommendations(product_name)
         return {"product_name": product_name, "recommendations": recommendations}
